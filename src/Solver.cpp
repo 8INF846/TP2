7fc898cac8870402c5ff16b5d8ec56305d952806
AmarOk <blin.sebastien@enconn.fr>
2016-10-20 10:43:58 -0400

    detect double values

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -1,6 +1,6 @@
 #include "Solver.h"
 #include <math.h>
-
+#include <algorithm>
 #include <iostream>
 
 Solver::Solver(Sudoku& sudoku) : m_sudoku(sudoku) {
@@ -36,6 +36,8 @@ bool Solver::solve() {
             auto cell = m_sudoku.getCell(r, c);
             auto possibleValues = cell.getPossiblesValues();
             //TODO here : MRV
+            if(possibleValues.size() == 0 && cell.getValue() == UNKNOWN)
+                throw std::string("can't solve this sudoku");
             if(possibleValues.size() == 1 && cell.getValue() == UNKNOWN) {
                 m_sudoku.setCellValue(r, c, possibleValues[0]);
                 removeConstraints(r, c, possibleValues[0]);
@@ -53,7 +55,11 @@ std::vector<int> Solver::getLineConstraints(unsigned int row) {
     std::vector<int> result;
     for (auto c = 0; c < m_sudoku.size(); ++c) {
         auto cell_value = m_sudoku.getCell(row, c).getValue();
-        if (cell_value != UNKNOWN) result.push_back(cell_value);
+        if (cell_value != UNKNOWN) {
+            if(std::find(result.begin(), result.end(), cell_value) != result.end())
+                throw std::string("2 values are identical in a row!");
+            result.push_back(cell_value);
+        }
     }
     return result;
 }
@@ -63,7 +69,11 @@ std::vector<int> Solver::getColumnConstraints(unsigned int column) {
     std::vector<int> result;
     for (auto r = 0; r < m_sudoku.size(); ++r) {
         auto cell_value = m_sudoku.getCell(r, column).getValue();
-        if (cell_value != UNKNOWN) result.push_back(cell_value);
+        if (cell_value != UNKNOWN) {
+            if(std::find(result.begin(), result.end(), cell_value) != result.end())
+                throw std::string("2 values are identical in a row!");
+            result.push_back(cell_value);
+        }
     }
     return result;
 }
@@ -80,7 +90,11 @@ std::vector<int> Solver::getBoxConstraints(unsigned int row, unsigned int col) {
     for (auto r = base_r; r < base_r + size_box; ++r) {
         for (auto c = base_c; c < base_c + size_box; ++c) {
             auto cell_value = m_sudoku.getCell(r, c).getValue();
-            if (cell_value != UNKNOWN) result.push_back(cell_value);
+            if (cell_value != UNKNOWN) {
+                if(std::find(result.begin(), result.end(), cell_value) != result.end())
+                    throw std::string("2 values are identical in a box!");
+                result.push_back(cell_value);
+            }
         }
     }
 

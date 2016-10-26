b7bfc339daffb3d6b656a785492271e0b25fa766
Victor Drouin Viallard <victor@drouin.io>
2016-10-25 20:06:07 -0400

    Change vector in Sudoku to array

--- src/Sudoku.cpp
+++ src/Sudoku.cpp
@@ -13,27 +13,28 @@ Sudoku::Sudoku(std::string sudoku_file) {
     std::string line;
     int width_grid = -1;
     std::string delimiter = ",";
+    unsigned int r = 0;
     while(std::getline(file, line)) {
-        m_grid.emplace_back();
         size_t pos = 0;
         int width = 0;
         std::string cell_value_str;
+        unsigned int c = 0;
         while ((pos = line.find(delimiter)) != std::string::npos) {
             cell_value_str = line.substr(0, pos);
-            if(cell_value_str == "") {
-                m_grid.back().push_back(Cell());
-            } else {
+            if(cell_value_str != "") {
                 int cell_value = std::stoi(cell_value_str);
-                m_grid.back().push_back(Cell(cell_value, (cell_value != UNKNOWN)));
+                m_grid[r][c] = Cell(cell_value, (cell_value != UNKNOWN));
             }
             ++width;
             line.erase(0, pos + delimiter.length());
+            ++c;
         }
         if(width_grid == -1) {
             width_grid = width;
             if(width % (int) std::sqrt(width) != 0) throw Except("Invalid size for sudoku");
         }
         else if(width_grid != width) throw Except("Invalid size for sudoku");
+        ++r;
     }
 
     for (auto r = 0; r < m_grid.size(); ++r) {
--- src/Sudoku.h
+++ src/Sudoku.h
@@ -2,7 +2,7 @@
 #define SUDOKU_H
 
 #include <string>
-#include <vector>
+#include <array>
 
 #include "Cell.h"
 
@@ -20,7 +20,7 @@ public:
     void setCellValue(unsigned int row, unsigned int col, int newValue);
 
 private:
-    std::vector<std::vector<Cell>> m_grid; //TODO std::array
+    std::array<std::array<Cell, 9>, 9> m_grid;
 };
 
 #endif //SUDOKU_H
